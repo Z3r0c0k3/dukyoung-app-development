629c500314c89f81fe43f0b77be4bf3e69a06240
zeroday2162 <zeroday2162@gmail.com>
2023-06-19 22:16:22 +0900

    진도 개많이 나갔네..

--- DATA/Kiosk_TUI/Kiosk_TUI.java
+++ DATA/Kiosk_TUI/Kiosk_TUI.java
@@ -5,10 +5,14 @@ class Kiosk_TUI {
 
     public static void main(String[] args){
         Kiosk_TUI myKiosk = new Kiosk_TUI();
+        Kiosk_TUI fuckKiosk = myKiosk;
+        fuckKiosk.hello_msg = "Fuck You";
+        fuckKiosk.cash = 100;
 
-        System.out.println( myKiosk.hello_msg );
         System.out.println( myKiosk.cash );
+        System.out.println( myKiosk.hello_msg );
         System.out.println( myKiosk.pay() );
+        System.out.println( myKiosk );
     }
 
     int pay(){
