ac619754240be63134374800bf31a451cad5cc82
zeroday2162 <zeroday2162@gmail.com>
2023-06-19 21:33:15 +0900

    중간 save

--- DATA/Kiosk_TUI/Kiosk_TUI.java
+++ DATA/Kiosk_TUI/Kiosk_TUI.java
@@ -1,10 +1,14 @@
 class Kiosk_TUI {
 
-    String hello_msg = "ㅎㅇ"
+    String hello_msg = "ㅎㅇ";
     int cash = 1000000;
 
     public static void main(String[] args){
+        Kiosk_TUI myKiosk = new Kiosk_TUI();
 
+        System.out.println( myKiosk.hello_msg );
+        System.out.println( myKiosk.cash );
+        System.out.println( myKiosk.pay() );
     }
 
     int pay(){
